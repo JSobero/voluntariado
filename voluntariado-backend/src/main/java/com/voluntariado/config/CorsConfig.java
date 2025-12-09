5a68a8e42807f4f89aefb4c48bb2a57ce7b6e95b
Joaquin Pomayay <joaquinpomayay2020@gmail.com>
2025-12-08 21:22:50 -0500

    arreglo de configuracion

--- voluntariado-backend/src/main/java/com/voluntariado/config/CorsConfig.java
+++ voluntariado-backend/src/main/java/com/voluntariado/config/CorsConfig.java
@@ -16,7 +16,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://majestic-creponne-8619c7.netlify.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
